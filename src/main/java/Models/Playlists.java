a42b9d253f09e099e1ea514313355c8564b423b3
Basel <basel.ihab77@gmail.com>
2018-03-15 16:47:25 +0200

    implemented postPlaylistByID

--- src/main/java/Models/Playlists.java
+++ src/main/java/Models/Playlists.java
@@ -6,7 +6,11 @@ import com.arangodb.entity.BaseDocument;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Playlists {
 
@@ -42,10 +46,9 @@ public class Playlists {
         return playlistObject.toString();
 
 
-
     }
 
-    public static String deletePlaylistByID ( int id, int subID){
+    public static String deletePlaylistByID(int id, int subID) {
         ArangoDB arangoDB = new ArangoDB.Builder().build();
         String dbName = "playlists";
         String collectionName = "firstPlaylist";
@@ -64,6 +67,84 @@ public class Playlists {
 
         return true + "";
     }
+
+    /**
+     * Add/Update a playlist in the database based on validity of input id
+     *
+     * @param id
+     * @param title
+     * @param channel_id
+     * @param description
+     * @param views_count
+     * @param videos_count
+     * @param privacy
+     * @param playlist_type
+     * @param videos
+     * @return
+     */
+    public static String postPlaylistByID(int id, String title, int channel_id, String description, int views_count, int videos_count, String privacy, String playlist_type, JSONArray videos) {
+        ArangoDB arangoDB = new ArangoDB.Builder().build();
+        //Check if document whit id exist
+        if (arangoDB.db(dbName).collection(collectionName).getDocument("" + id,
+                BaseDocument.class) == null) {
+            //It doesn't exist, create and add
+            //Create a new document
+            BaseDocument myObject = new BaseDocument();
+            //Add attributes
+            myObject.setKey(id + "");
+            myObject.addAttribute("videos", videos);
+            myObject.addAttribute("title", title);
+            myObject.addAttribute("channel_id", channel_id);
+            myObject.addAttribute("description", description);
+            myObject.addAttribute("views_count", views_count);
+            myObject.addAttribute("videos_count", videos_count);
+            myObject.addAttribute("privacy", privacy);
+            myObject.addAttribute("playlist_type", playlist_type);
+            String dateNow = getDateNowISO();
+            myObject.addAttribute("created_on", dateNow);
+            myObject.addAttribute("last_updated", dateNow);
+            //Try adding to database
+            try {
+                arangoDB.db(dbName).collection(collectionName).insertDocument(myObject);
+                System.out.println("Document created");
+            } catch (ArangoDBException e) {
+                System.err.println("Failed to create document. " + e.getMessage());
+            }
+            return myObject.toString();
+        } else {
+            //Document already exist so get it, update it and re-insert it
+            BaseDocument myDocument2 = arangoDB.db(dbName).collection(collectionName).getDocument("" + id,
+                    BaseDocument.class);
+            //Update the attributes needed
+            myDocument2.updateAttribute("videos", videos);
+            myDocument2.updateAttribute("title", title);
+            myDocument2.updateAttribute("channel_id", channel_id);
+            myDocument2.updateAttribute("description", description);
+            myDocument2.updateAttribute("views_count", views_count);
+            myDocument2.updateAttribute("videos_count", videos_count);
+            myDocument2.updateAttribute("privacy", privacy);
+            myDocument2.updateAttribute("playlist_type", playlist_type);
+            myDocument2.updateAttribute("last_updated", getDateNowISO());
+            //Delete old document from database
+            arangoDB.db(dbName).collection(collectionName).deleteDocument("" + id);
+            //Add the new one to the database
+            arangoDB.db(dbName).collection(collectionName).insertDocument(myDocument2);
+            return myDocument2.toString();
+        }
+    }
+
+    /**
+     * Get date now in ISO 8601 format
+     *
+     * @return
+     */
+    private static String getDateNowISO() {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+        df.setTimeZone(tz);
+        return df.format(new Date());
+    }
+
 }
 
 /*
